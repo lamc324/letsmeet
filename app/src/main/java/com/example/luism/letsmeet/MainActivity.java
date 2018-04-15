24ea21b2f54f8ada3f91b0d72418882f7f4bdbf3
lamc324 <luismorgan046@hotmail.com>
2018-04-14 22:48:05 -0600

    Muchas cosas

--- app/src/main/java/com/example/luism/letsmeet/MainActivity.java
+++ app/src/main/java/com/example/luism/letsmeet/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.luism.letsmeet;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,26 +27,93 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int ERROR_DIALOG_REQUEST = 9001;
 
+    private final static int LOGIN_PERMISSION=1000;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
         if(isServicesOK()){
-            init();
+            statusCheck();
         }
     }
 
     private void init(){
+        final List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
+
         Button btnMap = (Button) findViewById(R.id.btnMap);
         btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                startActivity(intent);
+                startActivityForResult(
+                        AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers).build(),LOGIN_PERMISSION
+
+                );
+
+                //Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                //startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == LOGIN_PERMISSION) {
+            startNewActivity(resultCode,data);
+        } else{
+           // startNewActivity(resultCode,data);
+        }
+    }
+
+    private void startNewActivity(int resultCode, Intent data){
+        if(resultCode == RESULT_OK){
+            Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+            startActivity(intent);
+            finish();
+
+        } else{
+            Toast.makeText(this,"Login failed !!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void statusCheck() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+
+        } else{
+            init();
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        //Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                        //startActivity(intent);
+                        //finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 
     public boolean isServicesOK(){
